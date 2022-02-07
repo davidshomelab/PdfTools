9b39d03153b463559a720bffc32154ed38445bc0
David Hollings <david@davidshomelab.com>
2022-02-07 21:44:29 +0000

    Add test module

---
+++ PdfLibTests/TestUtilities/DocumentValidation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using PdfSharp.Pdf.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PdfLibTests.TestUtilities
+{
+    public static class DocumentValidation
+    {
+        public static bool DocumentEmpty(string filePath)
+        {
+            FileInfo file = new FileInfo(filePath);
+
+            return file.Length == 0;
+        }
+
+        public static void DocumentReadable(string filePath)
+        {
+            PdfReader.Open(filePath);
+
+        }
+    }
+}
