91dea62807421194f479fb9a521335545a97a07b
David Hollings <david@davidshomelab.com>
2022-02-08 17:52:04 +0000

    Remove unnecessary using directives

--- PdfLibTests/PdfActionsTests.cs
+++ PdfLibTests/PdfActionsTests.cs
@@ -5,11 +5,6 @@ using PdfLibTests.TestUtilities;
 using PdfSharp;
 using PdfSharp.Pdf;
 using PdfSharp.Pdf.IO;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PdfLibTests
 {
